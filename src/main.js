8be64a99c501db5eaa23bd96aa5636e6b678a797
fernando <fernandoreal.valentim@gmail.com>
2023-01-28 18:48:47 -0300

    .

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import { downloadAudio } from "./downloader.js";
 
+let values = [
+  "https://www.youtube.com/watch?v=wpqm-05R2Jk",
+  "https://www.youtube.com/watch?v=gaA7RAy5rYg",
+  "https://www.youtube.com/watch?v=XpdpW0z9xnQ",
+  "https://www.youtube.com/watch?v=ZWijx_AgPiA",
+  "https://www.youtube.com/watch?v=5rAOyh7YmEc",
+  "https://www.youtube.com/watch?v=db5OQmod0JU",
+  "https://www.youtube.com/watch?v=db5OQmod0JU",
+  "https://www.youtube.com/watch?v=u6EuhylVHaw",
+  "https://www.youtube.com/watch?v=omwXLXeTR4w",
+];
 
-downloadAudio('https://www.youtube.com/watch?v=v8xowf-HNiY');
+for (let i = 0; i < values.length; i++) {
+    downloadAudio(values[i]);
+}
